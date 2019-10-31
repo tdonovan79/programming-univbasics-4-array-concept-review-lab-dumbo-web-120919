8a92a5ec06d927649dac818a26e630d830b26a0c
Thomas Donovan <tdonovan79@gmail.com>
2019-10-31 02:52:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,6 +11,7 @@ end
 
 def find_max_value(array)
   max = 0
+  counter = 0
   while counter < array.size do
     if array[counter] > max
       max = array[counter]
@@ -23,6 +24,7 @@ end
 
 def find_min_value(array)
   min = 0
+  counter = 0
   while counter < array.size do
     if array[counter] < min
       min = array[counter]
